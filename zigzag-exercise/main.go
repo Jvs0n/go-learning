e41262818c6e6e98c042b8e510d5c54a27478a18
Jvs0n <CHE220133@spengergasse.at>
2025-10-26 01:27:41 +0200

    First Exercise: ZigZag

---
+++ zigzag-exercise/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"unicode"
+)
+
+func main() {
+	var name = ""
+	fmt.Println("Converting your name into zigzag-case")
+	fmt.Print("Enter your name: ")
+	_, err := fmt.Scanln(&name)
+	if err != nil {
+		return
+	}
+	var result []rune
+	for index, character := range name {
+		if index%2 == 0 {
+			result = append(result, unicode.ToLower(character))
+		} else {
+			result = append(result, unicode.ToUpper(character))
+		}
+	}
+	zigzag := string(result)
+	print(zigzag)
+
+}
